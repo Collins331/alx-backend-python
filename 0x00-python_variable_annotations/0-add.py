43b1d37eaf3261ade3cbc49a84f807602b23c492
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-02 17:41:29 +0300

    string & float input

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    add function that takes 2 float arguments and return
+    their sum as float.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    """
+    accepts two string argumnets and concatinate them
+    returning a string
+    """
+    return str1 + str2
