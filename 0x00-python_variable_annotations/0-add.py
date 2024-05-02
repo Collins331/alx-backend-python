0265adbed623a79264d7a538665e9a1d1f8fa711
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-02 17:56:14 +0300

    string & float argument

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Write a type-annotated function add that takes a float a and a
+float b as arguments and returns their sum as a float.
+"""
+
 
 def add(a: float, b: float) -> float:
     """
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Write a type-annotated function add that takes a strings str1 and str2
+as arguments and returns their combination.
+"""
+
 
 def concat(str1: str, str2: str) -> str:
     """
